fa91c7a602f8f5d084eeaac0c17c22e33f858552
LanaCreegan <Lanacreegan17@gmail.com>
2023-07-25 14:36:21 +0000

    Create recipe model

--- foodblog/models.py
+++ foodblog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_blog")
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    favourites = models.ManyToManyField(User, related_name="recipe_favourites")
+
+
+class Meta: 
+    ordering = ['date_created_on']
+
+
+def __str__(self):
+    return self.title
