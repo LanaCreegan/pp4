cbf1a01885ddede460828d6aa44690b2876c28c9
LanaCreegan <Lanacreegan17@gmail.com>
2023-07-27 11:24:35 +0000

    Create recipe model

---
+++ recipes/models.py
@@ -0,0 +1,18 @@
+from django.db import models 
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    date_created_on = models.DateTimeField(auto_now_add=True)
+    date_updated_on = models.DateTimeField(auto_now=True)
+    description = models.TextField
+    featured_image = CloudinaryField('image', default='placeholder')
+
+
+    class Meta:
+        ordering = ['date_created_on']
+
+    def __str__(self):
+        return str(self.title)
