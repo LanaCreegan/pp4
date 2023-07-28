4a6de47bbcd3349bac78c8256a376466f0eda93d
LanaCreegan <Lanacreegan17@gmail.com>
2023-07-28 18:02:29 +0000

    Import datetime to recipe model and add slug

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,11 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     date_created_on = models.DateTimeField(auto_now_add=True)
     date_updated_on = models.DateTimeField(auto_now=True)
