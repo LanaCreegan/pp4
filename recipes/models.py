4de6911fdb801dfcd6cd12fa6bbe3c8ff9664195
LanaCreegan <Lanacreegan17@gmail.com>
2023-07-28 18:22:00 +0000

    Add tuple and datetime to recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -3,10 +3,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    now = datetime.datetime.now()
     date_created_on = models.DateTimeField(auto_now_add=True)
     date_updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField(max_length=500, null=True)
