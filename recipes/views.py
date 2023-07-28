41b294c32b13423969ee922d535df924ea239c3b
LanaCreegan <Lanacreegan17@gmail.com>
2023-07-28 18:24:00 +0000

    Add queryset to recipe view

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,6 @@ from .models import Recipe
 
 class Recipe(ListView):
     model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-date_created_on')
     template_name = 'recipes.html'
     paginate_by = 4
