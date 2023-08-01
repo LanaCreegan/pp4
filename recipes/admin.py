1809579d38fa57aaf2588289b2c4182a44cd915b
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-01 19:14:30 +0000

    Update recipe admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,5 +5,8 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-        
+        list_display = ('title', 'slug', 'status', 'date_created_on')
+        search_fields = ['title', 'content']
+        list_filter = ('status', 'date_created_on')
+        prepopulated_fields = {'slug': ('title',)}
         summernote_fields = ('description')
