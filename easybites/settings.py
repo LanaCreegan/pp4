52636f181375a04226de9a25c85be9599378a47b
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-01 09:57:53 +0000

    Install summernote and add url path

--- easybites/settings.py
+++ easybites/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'cloudinary',
+    'django_summernote',
     'foodblog',
     'home',
     'recipes',
--- easybites/urls.py
+++ easybites/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('recipes/', include('recipes.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
