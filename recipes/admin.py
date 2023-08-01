f5b75ea098699ec567e7e5ecc08bb0b0beaec408
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-01 19:30:40 +0000

    Add comment to admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -10,3 +10,14 @@ class RecipeAdmin(SummernoteModelAdmin):
         list_filter = ('status', 'date_created_on')
         prepopulated_fields = {'slug': ('title',)}
         summernote_fields = ('description')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'date_created_on', 'approved')
+    list_filter = ('approved', 'date_created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
