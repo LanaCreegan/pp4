cf0e0efacf41d154ff206e937d8599a993610e15
LanaCreegan <Lanacreegan17@gmail.com>
2023-08-08 12:29:01 +0000

    Add recipe like url

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipes_detail'),
+    path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipes_like')
 ]
 
